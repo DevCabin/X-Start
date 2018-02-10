e416be2b712bcf392ab0515232c9d163bb44f5d9
George Featherstone <george@devcabin.com>
2018-02-10 14:36:39 -0600

    Finish up minification of JS and edit functions.php to use it

--- functions.php
+++ functions.php
@@ -119,9 +119,10 @@ add_action( 'widgets_init', 'xstart_widgets_init' );
 function xstart_scripts() {
 	wp_enqueue_style( 'xstart-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'xstart-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// wp_enqueue_script( 'xstart-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// wp_enqueue_script( 'xstart-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'xstart-prod-javascript', get_template_directory_uri() . '/js/app.min.js', array(), '20180210', true );
 
-	wp_enqueue_script( 'xstart-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -155,4 +156,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
+// Thanks to https://www.sitepoint.com/wordpress-theme-automation-with-gulp/
+// and the WWW (well, Stackoverflow mostly)
+
 // We want to use cool es6 stuff
 require('es6-promise').polyfill();
-// Gotta Gulp it
-var gulp          = require('gulp');
+// Gotta Gulp it. The extra two are for minifying js for production
+var gulp          = require('gulp'),
+    gp_concat = require('gulp-concat'),
+    gp_uglify = require('gulp-uglify');
 // Freaking love SASS
 var sass          = require('gulp-sass');
 // Stupid broswer compatibility
 var autoprefixer  = require('gulp-autoprefixer');
 // Get useful error messages, not just blank fails on errors
 var plumber       = require('gulp-plumber');
+
+
 
 // start using that stuff
 gulp.task('sass', function() {
@@ -27,10 +34,31 @@ var onError = function (err) {
   console.log('An error occurred: ' + err.message);
   this.emit('end');
 };
+
+// get JS ready for production
+gulp.task('js', function(){
+    return gulp.src(['./js/*.js'])
+        .pipe(gp_concat('app.min.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('./js'));
+});
+
+// Note:
+// If you only need to include specific files, you can add them inside the gulp.src array as:
+// gulp.task('js', function() {
+//   return gulp.src([
+//     './js/navigation.js',
+//     './js/skip-link-focus-fix.js'
+//   ])
+// });
+
+
 // We don't want to run a command every time we change something,
 // this will do it automatically for us
 gulp.task('watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./js/*.js', ['js']);
 });
 // Define a default if we just type 'gulp' in the console with no argument
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
